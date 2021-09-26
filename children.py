f3577f52a0eaa7bc9baa944c6ded0091fa5054a6
AngelAlmaraz <64762942+AngelAlmaraz@users.noreply.github.com>
2021-09-26 01:57:59 -0500

    Update children.py

--- children.py
+++ children.py
@@ -9,8 +9,7 @@ if __name__ == '__main__':
     #Imprimimos el pid del padre
     print("Padre, pid = ",os.getpid())
     
-    #Creamos 10 procesos hijos, e impromimos su pid con la funcion
- info()
+    #Creamos 10 procesos hijos, e impromimos su pid con la funcion info()
     for i in range(10):
         p = Process(target=info, args=())
         p.start()
