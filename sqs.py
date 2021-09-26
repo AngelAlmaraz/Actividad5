5d7af0d9c838c34da372bb07b6cfe00bebd51b23
AngelAlmaraz <64762942+AngelAlmaraz@users.noreply.github.com>
2021-09-26 01:49:49 -0500

    Update sqs.py

--- sqs.py
+++ sqs.py
@@ -1,5 +1,6 @@
 
-
+#Ejemplo de LEARNAWS de como crear un queue de SQS
+#Es una funcion simple que crea una sqs queue usando la libreria boto3
 def create_queue():
     sqs_client = boto3.client("sqs", region_name="us-west-2")
     response = sqs_client.create_queue(
