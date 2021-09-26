6cab3d4a7c5e76b988c8974365d8bb7e7ecb49f1
AngelAlmaraz <64762942+AngelAlmaraz@users.noreply.github.com>
2021-09-26 01:35:48 -0500

    Update children.py

--- children.py
+++ children.py
@@ -8,10 +8,11 @@ def evaluate_forks(num):
         forks.append(False) if n<=0 else forks.append(True)
     return forks
 
+#Creamos una funcion para mostrar el pid de cada proceso
 def print_pid():
     print("Child: ", os.getpid())
 
-
+#Crearemos a un padre y a 10 hijos
 parent = [1,1,1,1]
 child1 = [0,1,1,1]
 child2 = [1,0,1,1]
@@ -24,6 +25,7 @@ child8 = [1,1,1,0]
 child9 = [0,1,1,0]
 child10 =[1,0,1,0]
 
+#Crearemos una lista vacia, en la que meteremos a los hijos 
 children = []
 
 children.append(child1)
@@ -53,7 +55,7 @@ forks = [n1,n2,n3,n4]
 res = evaluate_forks(forks)
 
 
-
+#iteramos la lista e imprimimos los pid de los hijos
 for child in children:
     if res == child[0:len(forks)]:
         print_pid()
