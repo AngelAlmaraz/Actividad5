79db7227729cc5adcab6dd5917a27ac177c17b1d
AngelAlmaraz <64762942+AngelAlmaraz@users.noreply.github.com>
2021-09-26 01:36:51 -0500

    Create children.py

---
+++ children.py
@@ -0,0 +1,20 @@
+from multiprocessing import Process
+import os
+
+#Creamos una funcion de info para ver el pid de los procesos hijos
+def info():
+    print("Child, pid = ", os.getpid())
+
+if __name__ == '__main__':
+    #Imprimimos el pid del padre
+    print("Padre, pid = ",os.getpid())
+    
+    #Creamos 10 procesos hijos, e impromimos su pid con la funcion
+ info()
+    for i in range(10):
+        p = Process(target=info, args=())
+        p.start()
+        p.join()
+~                                                                 
+~                                                                 
+~            
